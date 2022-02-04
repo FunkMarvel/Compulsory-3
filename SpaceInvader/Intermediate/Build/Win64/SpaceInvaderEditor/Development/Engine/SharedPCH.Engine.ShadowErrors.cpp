e7fc8eef9564af24b2b9353d477f4efdf1a7027d
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-02-04 14:15:45 +0100

    Cleaned up gitignore

--- SpaceInvader/Intermediate/Build/Win64/SpaceInvaderEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.30.30709
-#include "C:/Users/aande/OneDrive/Bilder/Dokumenter/Programming 2/Compulsory-2-SpaceInvader/SpaceInvader/Intermediate/Build/Win64/SpaceInvaderEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
