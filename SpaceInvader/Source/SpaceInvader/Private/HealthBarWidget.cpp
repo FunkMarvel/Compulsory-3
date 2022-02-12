0c022f4984984d3f04ee233b9be50938657d7070
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-02-12 22:13:06 +0100

    Finished Health bar

--- SpaceInvader/Source/SpaceInvader/Private/HealthBarWidget.cpp
+++ SpaceInvader/Source/SpaceInvader/Private/HealthBarWidget.cpp
@@ -2,4 +2,24 @@
 
 
 #include "HealthBarWidget.h"
+#include "Components/ProgressBar.h"
+#include "ShipPawn.h"
+#include "BaseEnemy.h"
 
+void UHealthBarWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime) {
+
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	if (!OwnerActor.IsValid()) return;
+
+	AActor* Owner = Cast<AActor>(OwnerActor);
+
+	if (Owner->IsA<AShipPawn>()) {
+		AShipPawn* OwnerShip = Cast<AShipPawn>(OwnerActor);
+		HealthBar->SetPercent(OwnerShip->Health/OwnerShip->MaxHealth);
+	}
+	//else if (Owner->IsA<ABaseEnemy>()) {
+	//	ABaseEnemy* OwnerEnemy = Cast<ABaseEnemy>(OwnerActor);
+	//	HealthBar->SetPercent(OwnerEnemy->Health/OwnerEnemy->MaxHealth * 100);
+	//}
+}
--- SpaceInvader/Source/SpaceInvader/Public/HealthBarWidget.h
+++ SpaceInvader/Source/SpaceInvader/Public/HealthBarWidget.h
@@ -12,6 +12,13 @@ class SPACEINVADER_API UHealthBarWidget : public UUserWidget
 	GENERATED_BODY()
 	
 protected:
+	TWeakObjectPtr<AActor> OwnerActor;
+
 	UPROPERTY(meta = (BindWidget))
 		class UProgressBar* HealthBar;
+
+	void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+public:
+	void SetOwnerOfBar(AActor* SomeActor) {OwnerActor = SomeActor; }
 };
