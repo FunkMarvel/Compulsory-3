3cd071b87bf96eb8362a1f2903ce440907b38dee
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-02-13 20:06:16 +0100

    Began working on stamina bars

--- SpaceInvader/Source/SpaceInvader/Private/DashBars.cpp
+++ SpaceInvader/Source/SpaceInvader/Private/DashBars.cpp
@@ -3,3 +3,10 @@
 
 #include "DashBars.h"
 
+UDashBars::UDashBars(const FObjectInitializer& ObjectInitializer) : UUserWidget(ObjectInitializer) {
+}
+
+void UDashBars::NativeConstruct() {
+	Super::NativeConstruct();
+
+}
--- SpaceInvader/Source/SpaceInvader/Public/DashBars.h
+++ SpaceInvader/Source/SpaceInvader/Public/DashBars.h
@@ -13,5 +13,17 @@ UCLASS()
 class SPACEINVADER_API UDashBars : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	UDashBars(const FObjectInitializer& ObjectInitializer);
+
+	virtual void NativeConstruct() override;
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+		class UProgressBar* StaminaBar1;
+	UPROPERTY(meta = (BindWidget))
+		class UProgressBar* StaminaBar2;
+	UPROPERTY(meta = (BindWidget))
+		class UProgressBar* StaminaBar3;
 };
