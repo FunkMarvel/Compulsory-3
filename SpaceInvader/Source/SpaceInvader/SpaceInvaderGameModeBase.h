e68c4d118221a764482d0a73af16361afbd1809b
Mathias M <mathmoe64@gmail.com>
2022-02-16 16:01:39 +0100

    small changes

--- SpaceInvader/Source/SpaceInvader/SpaceInvaderGameModeBase.h
+++ SpaceInvader/Source/SpaceInvader/SpaceInvaderGameModeBase.h
@@ -15,6 +15,38 @@ class SPACEINVADER_API ASpaceInvaderGameModeBase : public AGameModeBase
 	GENERATED_BODY()
 	
 protected:
+	ASpaceInvaderGameModeBase();
+
 	void BeginPlay() override final;
+
+	void Tick(float DeltaTime) override;
+
+	void SpawnEnemies(int32 EnemyType);
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Spawning|Enemies")
+		TSubclassOf<class ABaseEnemy> Basic;
+
+	UPROPERTY(EditAnywhere, Category = "Spawning|Enemies")
+		TSubclassOf<class ABaseEnemy> SharpShooter;
+
+	UPROPERTY(EditAnywhere, Category = "Spawning|Enemies")
+		TSubclassOf<class ABaseEnemy> Charger;
+
+	UPROPERTY(EditAnywhere, Category = "Spawning|Enemies")
+		TSubclassOf<class ABaseEnemy> Boss;
+
+	TArray<class ABaseEnemy*> EnemyArray;
+
+	bool bAllDead;
+
+	UPROPERTY(VisibleAnywhere, Category = "Spawning|Waves")
+		int32 NumberOfWaves{3};
+	UPROPERTY(EditAnywhere, Category = "Spawning|Waves")
+		int32 NumEnemiesWaveOne{3};
+	UPROPERTY(EditAnywhere, Category = "Spawning|Waves")
+		int32 NumEnemiesWaveTwo{3};
+	UPROPERTY(EditAnywhere, Category = "Spawning|Waves")
+		int32 NumEnemiesWaveThree{3};
 
 };
